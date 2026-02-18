1a24db9ae8438575f777f19f052b983e8a351dd2
ingarskrafts <ingarskrafts@gmail.com>
2026-02-18 05:27:23 -0500

    task 3/2

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,11 +9,17 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        total += cost * quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
